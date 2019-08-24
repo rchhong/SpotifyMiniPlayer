c74aef398129f19fb9e3dc7cc3052d0cf32cd37a
Ryan Chhong <ryanchhong@gmail.com>
2019-08-24 15:28:37 -0500

    Added progress bar

--- src/ui/App.jsx
+++ src/ui/App.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../css/App.css';
 
@@ -8,8 +8,7 @@ const spotifyApi = new SpotifyWebApi();
 // const { shell } = window.require('electron').remote;
 
 export default class App extends Component {
-  constructor(props)
-  {
+  constructor(props) {
     super(props);
 
   }
@@ -27,12 +26,12 @@ export default class App extends Component {
     }
     this.setState(
       {
-        view : false,
-        opacity : 1.0,
-        loggedIn : token ? true : false,
-        nowPlaying : { title : {name: 'Not Checked', url : ''}, albumArt: '', artists : []}
+        view: false,
+        opacity: 1.0,
+        loggedIn: token ? true : false,
+        dataReceived: false,
       }
-    );  
+    );
   }
 
   // componentDidMount() {
@@ -47,72 +46,82 @@ export default class App extends Component {
   //   clearInterval(this.timer);
   // }
 
-  getNowPlaying()
-  {
+  getNowPlaying() {
     spotifyApi.getMyCurrentPlaybackState().then((res) => {
       console.log(res);
       this.setState({
-        nowPlaying : {
-          title : {name : res.item.name, url : res.item.external_urls.spotify},
-          albumArt : res.item.album.images[0].url,
-          artists : res.item.artists,
+        dataReceived: true,
+        nowPlaying: {
+          title: { name: res.item.name, url: res.item.external_urls.spotify },
+          albumArt: res.item.album.images[0].url,
+          artists: res.item.artists,
+          progress : res.progress_ms,
+          length : res.item.duration_ms
         }
       });
     });
   }
 
-  handleMouseOverInfo()
-  {
-    console.log('mouse over')
+  handleMouseOverInfo() {
     this.setState({
-        view : true,
-        opacity : .5
-      });
-  }
-
-  handleMouseOutInfo()
-  {
-    console.log('mouse out');
-    this.setState({
-      view : false,
-      opacity : 1.0
+      view: true,
+      opacity: .5
     });
   }
 
-  render()
-  {
+  handleMouseOutInfo() {
+    this.setState({
+      view: false,
+      opacity: 1.0
+    });
+  }
+
+  render() {
     return (
       <div>
         <div className="wrapper">
           {!this.state.loggedIn && <a href="http://localhost:8888/login"> Login to Spotify</a>}
-          
-          <div className="bg">
-            <div>
-              <img style={{opacity : this.state.opacity}} onMouseEnter={this.handleMouseOverInfo.bind(this)} onMouseLeave={this.handleMouseOutInfo.bind(this)} className="image" src={this.state.nowPlaying.albumArt} alt="Album Art"/>
+          {this.state.dataReceived &&
+            <div className="bg">
+              <div>
+                <img style={{ opacity: this.state.opacity }} onMouseEnter={this.handleMouseOverInfo.bind(this)} onMouseLeave={this.handleMouseOutInfo.bind(this)} className="image" src={this.state.nowPlaying.albumArt} alt="Album Art" />
+              </div>
             </div>
-          </div>
-          {this.state.view && 
-                    <div className="info-wrapper">
-                    <div>
-                      <a className="text title"href={this.state.nowPlaying.title.url}>{this.state.nowPlaying.title.name}</a> 
-                    </div>
-                    <div>
-                      {this.state.nowPlaying.artists.map((item, index, arr) => {
-                        let text = index ===  arr.length -1 ? item.name : item.name + ", "; 
-                        return (<a className = "text artist" key={index} href={item.external_urls.spotify}>{text}</a>);
-                      })
-                      }
-                    </div>
-                </div>
           }
-      </div>
-      <div>
-            {this.state.loggedIn && 
-              <button onClick={() => this.getNowPlaying()}>
-                Check Now Playing
+
+          {this.state.dataReceived && this.state.view &&
+            <div className="info-wrapper">
+              <div>
+                <a className="text title" href={this.state.nowPlaying.title.url}>{this.state.nowPlaying.title.name}</a>
+              </div>
+              <div>
+                {this.state.nowPlaying.artists.map((item, index, arr) => {
+                  let text = index === arr.length - 1 ? item.name : item.name + ", ";
+                  return (<a className="text artist" key={index} href={item.external_urls.spotify}>{text}</a>);
+                })
+                }
+              </div>
+            </div>
+          }
+          {this.state.dataReceived && 
+            <div className="progressbar-wrapper">
+              <div style={{color: "white", marginRight: "5px"}}>{`${Math.floor(this.state.nowPlaying.progress/1000/60)}:${('00'+Math.round((this.state.nowPlaying.progress % 60000)/1000)).slice(-2)}`}</div>
+              <div className="progressbar-bg">
+                <div style={{ width: `${(this.state.nowPlaying.progress / this.state.nowPlaying.length) * 100}%`}} className="progressbar">
+                </div>
+              </div>
+              <div style={{color: "white", marginLeft : "5px"}}>{`${Math.floor(this.state.nowPlaying.length/1000/60)}:${('00'+Math.round((this.state.nowPlaying.length % 60000)/1000)).slice(-2)}`}</div>
+            </div>
+          }
+
+        </div>
+        <div>
+          {this.state.loggedIn &&
+            <button onClick={() => this.getNowPlaying()}>
+              Check Now Playing
               </button>
-            }
-          </div>
+          }
+        </div>
       </div>
     );
   }
